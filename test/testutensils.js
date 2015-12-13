e6bea1292f4b00b394889c22cb7461e8016bb44b
sverweij <sverweij@users.noreply.github.com>
2015-12-13 10:42:00 +0100

    [test] moves a global into the testutensils closure

--- test/testutensils.js
+++ test/testutensils.js
@@ -5,11 +5,11 @@ var chai   = require("chai");
 var expect = chai.expect;
 chai.use(require("chai-xml"));
 
-var gHashToUse = [ 'ripemd160', 'md5', 'sha1'].filter(function(h){
-    return crypto.getHashes().indexOf(h) > -1;
-})[0];
-
 module.exports = (function() {
+
+    var gHashToUse = [ 'ripemd160', 'md5', 'sha1'].filter(function(h){
+        return crypto.getHashes().indexOf(h) > -1;
+    })[0];
 
     function hashit(pString){
         return crypto.createHash(gHashToUse).update(pString).digest('hex');
@@ -25,73 +25,73 @@ module.exports = (function() {
         );
     }
     return {
-    assertequalToFileJSON : assertequalToFileJSON,
+        assertequalToFileJSON : assertequalToFileJSON,
 
-    assertequalFileJSON : function(pFoundFileName, pExpectedFileName){
-        assertequalToFileJSON(
-            pExpectedFileName,
-            JSON.parse(
-                fs.readFileSync(pFoundFileName, {"encoding":"utf8"})
-            )
-        );
-    },
-
-    assertequalToFile: function assertequalToFile(pExpectedFileName, pFoundFileName){
-        expect(
-            fs.readFileSync(pFoundFileName, {"encoding":"utf8"})
-        ).to.equal(
-            fs.readFileSync(pExpectedFileName, {"encoding":"utf8"})
-        );
-    },
-
-    assertequalFileXML : function (pFoundFileName, pExpectedFileName){
-        var lFound    = fs.readFileSync(pFoundFileName, {"encoding" : "utf8"});
-        var lExpected = fs.readFileSync(pExpectedFileName, {"encoding" : "utf8"});
-
-        expect(lFound).xml.to.be.valid();
-        expect(lFound).xml.to.deep.equal(lExpected);
-    },
-
-    assertequalProcessingXML : function(pExpectedFileName, pInputFileName, pProcessingFn){
-        var lProcessedInput   = pProcessingFn (
-            fs.readFileSync(pInputFileName, {"encoding" : "utf8"})
-        );
-
-        expect(lProcessedInput).xml.to.be.valid();
-        expect(
-            lProcessedInput
-        ).xml.to.deep.equal(
-            fs.readFileSync(pExpectedFileName, {"encoding" : "utf8"})
-        );
-    },
-
-    assertequalProcessing : function(pExpectedFileName, pInputFileName, pProcessingFn){
-        assert.equal(
-            hashit(
-                pProcessingFn(
-                    fs.readFileSync(pInputFileName, {"encoding" : "utf8"})
+        assertequalFileJSON : function(pFoundFileName, pExpectedFileName){
+            assertequalToFileJSON(
+                pExpectedFileName,
+                JSON.parse(
+                    fs.readFileSync(pFoundFileName, {"encoding":"utf8"})
                 )
-            ),
-            hashit(
-                fs.readFileSync(pExpectedFileName, {"encoding" : "utf8"})
-            )
-        );
-    },
+            );
+        },
 
-    assertSyntaxError: function(pProgram, pParser, pErrorType){
-        if (!pErrorType){
-            pErrorType = "SyntaxError";
-        }
-        try {
-            var lStillRan = false;
-            if (pParser.parse(pProgram)) {
-                lStillRan = true;
+        assertequalToFile: function assertequalToFile(pExpectedFileName, pFoundFileName){
+            expect(
+                fs.readFileSync(pFoundFileName, {"encoding":"utf8"})
+            ).to.equal(
+                fs.readFileSync(pExpectedFileName, {"encoding":"utf8"})
+            );
+        },
+
+        assertequalFileXML : function (pFoundFileName, pExpectedFileName){
+            var lFound    = fs.readFileSync(pFoundFileName, {"encoding" : "utf8"});
+            var lExpected = fs.readFileSync(pExpectedFileName, {"encoding" : "utf8"});
+
+            expect(lFound).xml.to.be.valid();
+            expect(lFound).xml.to.deep.equal(lExpected);
+        },
+
+        assertequalProcessingXML : function(pExpectedFileName, pInputFileName, pProcessingFn){
+            var lProcessedInput   = pProcessingFn (
+                fs.readFileSync(pInputFileName, {"encoding" : "utf8"})
+            );
+
+            expect(lProcessedInput).xml.to.be.valid();
+            expect(
+                lProcessedInput
+            ).xml.to.deep.equal(
+                fs.readFileSync(pExpectedFileName, {"encoding" : "utf8"})
+            );
+        },
+
+        assertequalProcessing : function(pExpectedFileName, pInputFileName, pProcessingFn){
+            assert.equal(
+                hashit(
+                    pProcessingFn(
+                        fs.readFileSync(pInputFileName, {"encoding" : "utf8"})
+                    )
+                ),
+                hashit(
+                    fs.readFileSync(pExpectedFileName, {"encoding" : "utf8"})
+                )
+            );
+        },
+
+        assertSyntaxError: function(pProgram, pParser, pErrorType){
+            if (!pErrorType){
+                pErrorType = "SyntaxError";
             }
-            expect(lStillRan).to.equal(false);
-        } catch(e) {
-            expect(e.name).to.equal(pErrorType);
+            try {
+                var lStillRan = false;
+                if (pParser.parse(pProgram)) {
+                    lStillRan = true;
+                }
+                expect(lStillRan).to.equal(false);
+            } catch(e) {
+                expect(e.name).to.equal(pErrorType);
+            }
         }
-    }
-};
+    };
 
 })();
