bbe85455d8e9cc85523a5db7f4854d520fb85bab
sverweij <sverweij@users.noreply.github.com>
2018-03-02 20:24:27 +0100

    1.15.1

--- main/index.js
+++ main/index.js
@@ -98,7 +98,7 @@ define(function(require){
             }
         },
 
-        version: "1.15.0",
+        version: "1.15.1",
 
         getAllowedValues: function() {
             return Object.freeze({
