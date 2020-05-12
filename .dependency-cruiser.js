a33c42e67720738f2c76522c9686d891b9c215fd
sverweij <sverweij@users.noreply.github.com>
2020-05-12 18:23:07 +0200

    build(npm): :arrow_up: @types/node, dependency-cruiser, jest, ts-loader

--- .dependency-cruiser.js
+++ .dependency-cruiser.js
@@ -7,22 +7,22 @@ module.exports = {
         "Don't allow dependencies between the different types of renderers",
       severity: "error",
       from: {
-        path: "^src/render/graphics|^src/render/astmassage"
+        path: "^src/render/graphics|^src/render/astmassage",
       },
       to: {
-        path: "^src/render/text/"
-      }
+        path: "^src/render/text/",
+      },
     },
     {
       name: "not-to-test",
       comment: "Don't allow dependencies from outside the test folder to test",
       severity: "error",
       from: {
-        pathNot: "^test"
+        pathNot: "^test",
       },
       to: {
-        path: "^test"
-      }
+        path: "^test",
+      },
     },
     {
       name: "not-from-test-to-dist",
@@ -31,11 +31,11 @@ module.exports = {
       severity: "error",
       from: {
         path: "^test",
-        pathNot: "^test/dist-index\\.spec\\.js"
+        pathNot: "^test/dist-index\\.spec\\.js",
       },
       to: {
-        path: "dist"
-      }
+        path: "dist",
+      },
     },
     {
       name: "not-to-spec",
@@ -44,8 +44,8 @@ module.exports = {
       severity: "error",
       from: {},
       to: {
-        path: "\\.spec\\.[js|ts]$"
-      }
+        path: "\\.spec\\.[js|ts]$",
+      },
     },
     {
       name: "not-to-dev-dep",
@@ -53,11 +53,11 @@ module.exports = {
       comment:
         "Don't allow dependencies from src/app/lib to a development only package",
       from: {
-        path: "^src"
+        path: "^src",
       },
       to: {
-        dependencyTypes: ["npm-dev"]
-      }
+        dependencyTypes: ["npm-dev"],
+      },
     },
     {
       name: "optional-deps-used",
@@ -66,8 +66,8 @@ module.exports = {
         "We're not working with optional dependencies - don't make sense for mscgenjs",
       from: {},
       to: {
-        dependencyTypes: ["npm-optional"]
-      }
+        dependencyTypes: ["npm-optional"],
+      },
     },
     {
       name: "peer-deps-used",
@@ -75,40 +75,40 @@ module.exports = {
       severity: "error",
       from: {},
       to: {
-        dependencyTypes: ["npm-peer"]
-      }
+        dependencyTypes: ["npm-peer"],
+      },
     },
     {
       name: "no-unreachable-from-api",
       comment: "All sources should be reachable from the API entry point",
       from: {
-        path: "^src/index\\.ts$|^src/index-lazy\\.ts$"
+        path: "^src/index\\.ts$|^src/index-lazy\\.ts$",
       },
       to: {
         path: "^src/",
         pathNot: "\\.d\\.ts$|/mscgenjs-ast\\.schema\\.json$",
-        reachable: false
-      }
+        reachable: false,
+      },
     },
     {
       name: "no-uncovered",
       comment: "All sources should be reachable from at least one test",
       severity: "error",
       from: {
-        path: "^test/^[\\.]+/\\.spec\\.ts$"
+        path: "^test/^[\\.]+/\\.spec\\.ts$",
       },
       to: {
         path: "^src/",
-        reachable: false
-      }
-    }
+        reachable: false,
+      },
+    },
   ],
   options: {
     moduleSystems: ["cjs", "es6"],
     // doNotFollow is already implied in the 'recommended-strict' config
     // "doNotFollow": "node_modules",
     tsPreCompilationDeps: "specify",
-    prefix: "https://github.com/mscgenjs/mscgenjs-core/blob/develop/",
+    prefix: "https://github.com/mscgenjs/mscgenjs-core/blob/master/",
     reporterOptions: {
       dot: {
         collapsePattern: "^node_modules/[^/]+",
@@ -116,41 +116,41 @@ module.exports = {
           modules: [
             {
               criteria: { source: "^src/main" },
-              attributes: { fillcolor: "#ccccff" }
+              attributes: { fillcolor: "#ccccff" },
             },
             {
               criteria: { source: "^src/render" },
-              attributes: { fillcolor: "#ccffcc" }
+              attributes: { fillcolor: "#ccffcc" },
             },
             {
               criteria: { source: "^src/parse" },
-              attributes: { fillcolor: "#ffccff" }
+              attributes: { fillcolor: "#ffccff" },
             },
             {
               criteria: { source: "parser\\.js$" },
-              attributes: { style: "filled" }
+              attributes: { style: "filled" },
             },
             {
               criteria: { source: "\\.json$" },
-              attributes: { shape: "cylinder" }
-            }
+              attributes: { shape: "cylinder" },
+            },
           ],
           dependencies: [
             {
               criteria: { resolved: "^src/main" },
-              attributes: { color: "#0000ff77" }
+              attributes: { color: "#0000ff77" },
             },
             {
               criteria: { resolved: "^src/render" },
-              attributes: { color: "#00770077" }
+              attributes: { color: "#00770077" },
             },
             {
               criteria: { resolved: "^src/parse" },
-              attributes: { color: "#ff00ff77" }
-            }
-          ]
-        }
-      }
-    }
-  }
+              attributes: { color: "#ff00ff77" },
+            },
+          ],
+        },
+      },
+    },
+  },
 };
